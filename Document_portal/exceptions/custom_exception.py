b135e9f6c51ce66481a17746dbd8a65eb05dcfcb
Tapas <tapaskumarpaul84@gmail.com>
2025-08-01 01:40:56 +0530

    updated

--- Document_portal/exceptions/custom_exception.py
+++ Document_portal/exceptions/custom_exception.py
@@ -1,1 +1,30 @@
-print('all ok')
+import traceback
+import sys
+from logger.custom_logger import CustomLogger
+
+logger=CustomLogger().get_logger(__file__)
+class DocumentPortalException(Exception):
+    """Custom Exception for Document Portal"""
+    def __init__(self,error_message:str,error_detail:sys):
+        _,_,exc_tb=error_detail.exc_info()
+        self.filename=exc_tb.tb_frame.f_code.co_filename
+        self.lineno=exc_tb.tb_lineno
+        self.error_message=str(error_message)
+        self.traceback_str="".join(traceback.format_exception(*error_detail.exc_info()))
+        
+    def __str__(self):
+        return f"""
+            Error in [{self.filename}] at line [{self.lineno}]
+            Message: {self.error_message}
+            Traceback: {self.traceback_str}
+        
+        """
+
+if __name__=="__main__":
+    try:
+        a=1/0
+        
+    except Exception as e:
+        app_exc=DocumentPortalException(e,sys)
+        logger.error(app_exc)
+        raise app_exc
---
+++ Document_portal/logger/custom_logger.py
@@ -0,0 +1,24 @@
+import logging
+import os
+from datetime import datetime
+
+class CustomLogger:
+    def __init__(self,log_dr='logs'):
+        self.logs_dir=os.path.join(os.getcwd(),log_dr)
+        os.makedirs(self.logs_dir,exist_ok=True)
+        self.LOG_FILE=f"{datetime.now().strftime('%m-%d-%Y_%H_%M_%S')}.log"
+        LOG_FILE_PATH=os.path.join(self.logs_dir,self.LOG_FILE)
+
+        logging.basicConfig(
+            filename=LOG_FILE_PATH,
+            format="[ %(asctime)s] %(levelname)s %(name)s (line: %(lineno)d) - %(message)s",
+            level=logging.INFO
+)
+    def get_logger(self,name=__file__):
+        return logging.getLogger(os.path.basename(name))
+    
+
+if __name__=="__main__":
+    logger=CustomLogger()
+    logger=logger.get_logger(__file__)
+    logger.info("Custom logger initialized")
