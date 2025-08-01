ce2344af3150f5d8a6073bd18fcf5e3031a923e5
Tapas <tapaskumarpaul84@gmail.com>
2025-08-01 23:28:58 +0530

    data analysis added

--- Document_portal/prompts/prompt_library.py
+++ Document_portal/prompts/prompt_library.py
@@ -1,1 +1,14 @@
-print('ok')
+from langchain_core.prompts import ChatPromptTemplate
+
+
+
+prompt=ChatPromptTemplate.from_template("""
+    You are a highly capable assistant trained to analyze and summerize documents.
+    Return ONLY valid JSON matching the exact schema below.
+                                        
+    {format_instructions}
+    
+    Analyze this document:
+    {document_text}
+
+    """)
